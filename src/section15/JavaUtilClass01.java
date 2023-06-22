2c841156afbf43cf1a916a490648945516770462
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-06-22 18:53:40 +0900

    section15

---
+++ src/section15/JavaUtilClass01.java
@@ -0,0 +1,29 @@
+package section15;
+/*
+ * 자바유틸패키지(java.util)
+ * 	자바에서 유용한 유틸클래스, 인터페이스를 포함하는 패키지
+ * 
+ * 
+ * Random 클래스
+ * 	난수 생성해주는 클래스
+ */
+
+import java.util.Random;
+
+public class JavaUtilClass01 {
+	public static void main(String[] args) {
+		Random ran = new Random();
+		// 0~99 사이 정수중 난수 발생
+		int ranNum = ran.nextInt(100);
+		System.out.println("난수: " + ranNum);
+		
+		// 0 이상 1미만 double값으로 반환
+		if(ran.nextDouble() < 0.5) {
+			System.out.println("강화에 성공하였습니다.");
+		}else {
+			System.out.println("강화에 실패하였습니다.");
+		}
+	}
+}
+
+
