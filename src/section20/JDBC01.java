db4bce7e028eb48dc7c04fa75d48b7cfbdc091b9
ITSC <ITSC@DESKTOP-PMHM7D7>
2023-07-04 19:48:51 +0900

    jdbc

--- src/section20/JDBC01.java
+++ src/section20/JDBC01.java
@@ -18,27 +18,27 @@ import java.sql.Statement;
  * 
  */
 public class JDBC01 {
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		
 		try {
-			// 1. 드라이버 클래스 정보 로딩
+			// 1. 드리이버 클래스 정보 로딩
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			
 			// 2. 데이터베이스 연결
 			String url = "jdbc:oracle:thin:@localhost:21521:xe";
 			conn = DriverManager.getConnection(url, "hr", "hr");
 			
-			// 3. 쿼리 수행을 위한 Statement 객체 생성
+			// 3. 쿼리 수행을 위해 Statement 객체 생성
 			stmt = conn.createStatement();
 			
 			// 4. SQL 쿼리작성
 			String param = "30";
 			String sql = "SELECT * "
 					+ "FROM employees "
-					+ "WHERE department_id=" + param;
+					+ "WHERE department_id =" + param;
 			
 			// 5. 쿼리 수행
 			rs = stmt.executeQuery(sql);
@@ -48,17 +48,30 @@ public class JDBC01 {
 				String firstName = rs.getString("first_name");
 				String lastName = rs.getString("last_name");
 				
-				System.out.println(employeeId+"/"+firstName+"/"+lastName);
+				System.out.println(employeeId + "/" + firstName + "/" + lastName);
 			}
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
-			if(rs !=null)rs.close();
-			if(stmt != null)stmt.close();
-			if(conn != null)conn.close();
+			try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(conn != null) conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
 		}
-		
 	}
-
 }
+
+
+
+
+
+
+
+
+
+
+
