6c4d75e53c20b9d187c90b041a60aea7afb8506e
jemicom <jemicom@gmail.com>
2023-09-19 20:22:40 +0900

    숙제 정답

--- src/section05/LoopHomeWork.java
+++ src/section05/LoopHomeWork.java
@@ -25,14 +25,14 @@ package section05;
 ******* 
   
 4번
-			
-   *   		
-  ***		
- *****		
-*******		
- *****		
-  ***		
-   *		
+			i		j
+   *   		0       4 5 6   j-i > 3
+  ***		1       5 6     j-i > 3
+ *****		2       6       j-i > 3
+*******		3       
+ *****		4       6       i+j > 9  
+  ***		5       5 6     i+j > 9
+   *		6       4 5 6   i+j > 9
  
 5번 구구단
 	 2 x 1 = 2 	3 x 1 = 3	4 x 1 = 4
@@ -40,5 +40,62 @@ package section05;
 
  */
 public class LoopHomeWork {
-
+	
+	public static void main(String[] args) {
+		
+//		for(int i = 0; i < 7; i++) {
+//			for(int j = 0; j < 7; j++) {
+//				if(i < 4) {
+//					if (i + j < 3) {
+//						System.out.print(" ");
+//					} else if(j - i > 3) {
+//						System.out.print(" ");
+//					} else {
+//						System.out.print("*");
+//					}
+//				} else {
+//					if (i - j > 3) {
+//						System.out.print(" ");
+//					} else if (i + j  > 9) {
+//						System.out.print(" ");
+//					} else {
+//						System.out.print("*");
+//					}
+//				}
+//			}
+//			
+//			System.out.println();	// 개행
+//		}
+		
+		System.out.println("   ☆");
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 7; j++) {
+				if (i + j < 3) {
+					System.out.print(" ");
+				} else if(j - i > 3) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();	// 개행
+		}
+		System.out.println("  | | Merry Christmas~*");
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/section05/LoopHomeWork2.java
+++ src/section05/LoopHomeWork2.java
@@ -32,5 +32,40 @@ package section05;
 
 */
 public class LoopHomeWork2 {
-
+	
+	public static void main(String[] args) {
+		
+		for(int k = 0; k < 3; k++) {
+			for(int i = 0; i < 9; i++) {
+				for(int j = 0; j < 3; j++) {
+					int dan = j + 2 + (k * 3);
+					int num = i + 1;
+					
+					if(dan > 9) break;
+					System.out.print(dan+"x"+num+"="+dan * num+"\t");
+					
+//					if(dan < 10) {
+//						System.out.print(dan+"x"+num+"="+dan * num+"\t");
+//					}
+					
+				}
+				System.out.println(); // 개행
+			}
+			System.out.println();
+		}
+		
+		
+		
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
