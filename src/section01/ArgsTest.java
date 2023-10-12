8368ee6ebf72ec7ded568ef1aec26df3cf3f15cc
jemicom <jemicom@gmail.com>
2023-10-12 22:22:05 +0900

    sec 13 14 15

---
+++ src/section01/ArgsTest.java
@@ -0,0 +1,13 @@
+package section01;
+
+public class ArgsTest {
+	
+	public static void main(String[] args) {
+		
+		for(String str : args) {
+			System.out.println(str);
+		}
+		
+	}
+
+}
--- src/section13/OuterClass07.java
+++ src/section13/OuterClass07.java
@@ -3,8 +3,8 @@ package section13;
 import section13.access2.BirdImpl;
 
 public class OuterClass07 {
+	
 	public static void main(String[] args) {
-		
 		
 		String name = new BirdImpl().getName();
 		
