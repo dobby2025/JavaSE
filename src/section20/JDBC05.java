e41dd6291faec7d6fcc69f6c86807b79ad0b3ca6
dobby2025 <128953898+dobby2025@users.noreply.github.com>
2023-10-31 22:23:17 +0900

    jdbc

--- src/section20/JDBC05.java
+++ src/section20/JDBC05.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JDBC04 {
+public class JDBC05 {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = null;
 		// Statement 사용하지 않기!! PrepareStatement 사용하기!!! 
@@ -19,23 +19,20 @@ public class JDBC04 {
 			// 2. 데이터베이스 연결
 			String url = "jdbc:oracle:thin:@localhost:31521:xe";
 			conn = DriverManager.getConnection(url, "hr", "hr");
-			
 					
 			// 3. 쿼리 작성
 			pstmt = conn.prepareStatement(
-					"UPDATE sales_reps SET "
-					+ "name = ? "
+					"DELETE FROM sales_reps "
 					+ "WHERE id = ?"
 					);
 			
-			pstmt.setString(1, "리자몽");
-			pstmt.setString(2, "1");
+			pstmt.setInt(1, 1);
 			
 			int reuslt = pstmt.executeUpdate();
 			if(reuslt > 0) {
-				System.out.println("데이터 UPDATE 성공!");
+				System.out.println("데이터 DELETE 성공!");
 			} else {
-				System.out.println("데이터 UPDATE 실패!");
+				System.out.println("데이터 DELETE 실패!");
 			}
 			
 		} catch(Exception e) {
--- src/section20/JDBC06.java
+++ src/section20/JDBC06.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class JDBC04 {
+public class JDBC06 {
 	public static void main(String[] args) throws SQLException {
 		Connection conn = null;
 		// Statement 사용하지 않기!! PrepareStatement 사용하기!!! 
@@ -20,27 +20,42 @@ public class JDBC04 {
 			String url = "jdbc:oracle:thin:@localhost:31521:xe";
 			conn = DriverManager.getConnection(url, "hr", "hr");
 			
-					
+			// 트랜잭션 시작
+			conn.setAutoCommit(false);
+			
 			// 3. 쿼리 작성
 			pstmt = conn.prepareStatement(
-					"UPDATE sales_reps SET "
-					+ "name = ? "
-					+ "WHERE id = ?"
+					"INSERT INTO sales_reps VALUES (2, '라이츄', 200, 0.2)"
 					);
+			pstmt.executeUpdate();
 			
-			pstmt.setString(1, "리자몽");
-			pstmt.setString(2, "1");
+			pstmt = conn.prepareStatement(
+					"INSERT INTO sales_reps VALUES (3, '야도란', 300, 0.1)"
+					);
+			pstmt.executeUpdate();
 			
-			int reuslt = pstmt.executeUpdate();
-			if(reuslt > 0) {
-				System.out.println("데이터 UPDATE 성공!");
-			} else {
-				System.out.println("데이터 UPDATE 실패!");
-			}
+			pstmt = conn.prepareStatement(
+					"INSERT INTO sales_reps VALUES (4, '파이리', 400, 0.4)"
+					);
+			pstmt.executeUpdate();
+			
+			pstmt = conn.prepareStatement(
+					"INSERT INTO sales_reps VALUES (5, '잠만보', 900, 0.3)"
+					);
+			pstmt.executeUpdate();
+			
+			isSuccess = true;
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 		} finally {
+			if(isSuccess) {
+				conn.commit();
+				System.out.println("트랜잭션 처리 성공!");
+			} else {
+				conn.rollback();
+				System.out.println("트랜잭션 처리 실패!");
+			}
 			
 			if(pstmt != null) pstmt.close();
 			if(conn != null) conn.close();
